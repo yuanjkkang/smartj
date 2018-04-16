d5fed21b66e1b87fa25a3a4935e1a01e34bba305
dongdaiming <dongdaiming@socian.com.cn>
2018-04-16 18:40:41 +0800

    json

--- src/main/java/org/stathry/smartj/commons/utils/SmartJsonUtils.java
+++ src/main/java/org/stathry/smartj/commons/utils/SmartJsonUtils.java
@@ -1,9 +1,14 @@
 package org.stathry.smartj.commons.utils;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 /**
  * TODO
@@ -15,6 +20,10 @@ public class SmartJsonUtils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SmartJsonUtils.class);
     private static final String  EMPTY_JSON = "{}";
+    private static final String Q = "\"";
+    private static final String C = ",";
+    private static final String  SEP = ":";
+    private static final String  L = SystemUtils.LINE_SEPARATOR;
 
 
     private SmartJsonUtils() {}
@@ -24,12 +33,55 @@ public class SmartJsonUtils {
             return EMPTY_JSON;
         }
         String r = EMPTY_JSON;
+        Object json = null;
         try {
-            JSON.parse(src);
+            json = JSON.parse(src);
         } catch (Exception e) {
             LOGGER.error("invalid json:{}", src);
             LOGGER.error("parse json error.", e);
         }
+        if(json == null) {
         return r;
+        }
+
+        StringBuilder b = new StringBuilder();
+        append(b, json);
+        return b.toString();
+    }
+
+    private static void append(StringBuilder b, Object json) {
+        if(json.getClass() == JSONObject.class) {
+            JSONObject o = (JSONObject)json;
+            b.append('{');
+            b.append(L);
+            for(Map.Entry<String, Object> e : o.entrySet()) {
+                appendKV(b, e);
+            }
+            b.deleteCharAt(b.lastIndexOf(C));
+            b.append('}');
+        } else {
+            b.append('[');
+            JSONArray a = (JSONArray) json;
+            b.append(']');
+        }
+    }
+
+    private static void appendKV(StringBuilder b, Map.Entry<String, Object> e) {
+        b.append(Q);
+        b.append(e.getKey());
+        b.append(Q);
+        b.append(SEP);
+        b.append(Q);
+        if(e.getValue().getClass() == JSONObject.class) {
+
+        }
+        b.append(e.getValue());
+        b.append(Q);
+        b.append(C);
+        b.append(L);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(format("{\"k1\":\"v1\", \"k2\":\"v2\"}"));
     }
 }
